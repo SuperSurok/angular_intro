19ad20698fdd0ca202f9d403ed3f380385d491a5
Sergey <lebedev_81@magora-systems.com>
2018-10-10 10:57:30 +0300

    Added directives: ngFor, ngIf, ngClass.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,19 @@
 import {Component} from "@angular/core";
 
+const todos = [
+    'Изучить JavaScript',
+    'Изучить Angular 2',
+    'Написать приложение'
+];
+
 @Component({
     moduleId: module.id,
     selector: 'app',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-    title = 'Angular to do'
+    title = 'Angular to do';
+    todos = todos;
 }
